a8f30b52801b868d39d6f3f11f1459a5be5f5fe4
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-04-01 14:51:37 +0700

    input no mahasiswa ( NIM ) dan nama Mahasiswa

--- strukturC_2.cpp
+++ strukturC_2.cpp
@@ -12,3 +12,10 @@ struct Mahasiswa {
     DetailAlamat alamat;
     int umur;
 };
+
+int main(){
+    Mahasiswa mhs;
+    cout << " Nomor Mahasiswa : ";
+    cin >> mhs.nim;
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
