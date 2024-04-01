48b7c0fce2e2a5511d0aa13ffbbe5dae263ee9d7
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-04-01 15:51:05 +0700

    menampilkan struct detail alamat dan mahasiswa

---
+++ strukturC_4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+struct DetailAlamat{
+    string kota;
+    string desa;
+};
+
+struct Mahasiswa{
+    string nim;
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+};
+
